31d187924388507016fa517f588c55d48d6b761a
Erin Land <erinland05@gmail.com>
2018-01-04 20:39:19 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,5 @@ class Song < ActiveRecord::Base
   validates :title, :artist_name, presence: true
   validates :title, uniqueness: { :scope => [:artist_name, :release_year], message: "song titles cannot be repeated by the same artist in the same year"}
   validates :released, inclusion: { in: [true, false] }
-  
+
 end
